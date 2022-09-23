c1a1fcb64a1fb5378ef42f28a16b384906230a45
gepassos <gustavo.escuderopassos@gmail.com>
2022-09-23 01:45:34 -0400

    newcommit

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Main from "../template/Main";
+
+export default props =>
+    <Main icon="home" title="Ínicio"
+        subtitle="Segundo Projeto" >
+        <div className="display-4">Bem Vindo!</div>
+        <hr />
+        <p className="mb-0">Sistema feito em React JS</p>
+    </Main>
---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,17 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+<footer className="footer">
+    <span>
+        <b> Desenvolvido por <i className="  fa fa-flash text-danger">
+            <strong>
+            Gustavo 
+            <span className="fa fa-flash">
+               </span>
+            </strong></i>
+            </b>
+
+ 
+    </span>
+</footer>
